12b86a21be5d80f563be64feca72f9d29692cc76
maciek <mrobacki@gmail.com>
2021-08-25 11:41:46 +0200

    script optimization

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
-const button = document.createElement('button');
-button.innerHTML = 'klikaj';
-button.classList.add('btn');
-document.body.appendChild(button);
-button.style.display = 'block';
+const btn = document.createElement('button');
+
+const createBtn = (btnLabel) => {
+    btn.innerHTML = btnLabel;
+    btn.classList.add('btn');
+    document.body.appendChild(btn);
+}
 
 i = 1;
 
-button.addEventListener('click', function dodawanie() {
+const addElement = () => {
     const block = document.createElement('div');
     block.innerHTML = i;
     block.classList.add('block');
     document.body.append(block);
     if(i % 5 == 0) {
         block.classList.add('circle');
-    }
-    i++;
-});
+    };
+    i++
+};
+
+createBtn('Click here');
+
+btn.addEventListener('click', addElement);
